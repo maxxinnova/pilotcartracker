7fd174152c013c7e63d4ad1f427f52c365374737
Dietrich Westbrooks <westbrod001@icloud.com>
2018-03-22 20:53:32 -0500

    prepare project for travis

---
+++ Publisher/MaxxInnova.PilotCarTracker.Publisher/copyStaticAssets.ts
@@ -0,0 +1,5 @@
+﻿var shell = require("shelljs");
+
+shell.cp("-R", "src/public/js/lib", "public/js/");
+shell.cp("-R", "src/public/fonts", "public/");
+shell.cp("-R", "src/public/images", "public/");
